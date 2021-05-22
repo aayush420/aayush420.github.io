71509feec80c1c31b66f54e9928fa284ea0aad21
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2021-05-22 18:12:39 +0530

    Basic navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div class="bg-red-500 text-red-100">Hello from Tailwind again and again!!</div>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Nav1></Nav1>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <div class="bg-red-500 text-red-100">Hello from Tailwind again and again!!</div> -->
+  <div id="home" style="display: grid; height: 80vh; place-items: center">
+    <div class="leading-relaxed text-7xl">
+      Nothing<br />[ worth having ]<br />comes easy
+    </div>
+  </div>
+  <div id="projects">
+    Projects
+    <div id="dynamic-projects">
+      Dynamic Projects
+    </div>
+    <div id="static-projects">
+      Static Projects
+    </div>
+  </div>
+  <div id="about">
+    About Me
+  </div>
+  <div id="contact">
+    My Contact Info
+  </div>
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Nav1 from "./components/Nav.vue";
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
@@ -18,6 +40,6 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header fixed w-full bg-gray-800 text-gray-50 pt-4 p-2">
+    <div class="wrap">
+      <div class="h-8 inline absolute left-0 logo"><a href="#"><img src="../assets/logo.png" class="h-full inline-block px-4" /> <span class="text-xl">Balakrishna Prasad Ganne</span></a></div>
+
+      <a class="inline-block" id="menu-icon">&#9776; Menu</a>
+
+      <nav class="navbar">
+        <ul class="menu">
+          <li v-for="i in links" :key="i.link"><a class="" :href="i.link" v-text="i.text"></a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {text:"Home", link:"#home"},
+                {text:"Projects", link:"#projects"},
+                {text:"About", link:"#about"},
+                {text:"Contact", link:"#contact"}
+            ]
+        }
+    }
+}
+</script>
