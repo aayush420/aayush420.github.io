4fe170707f5aa3b91665588d30f19cfa595361d8
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2021-05-22 21:20:01 +0530

    Added tabs for projects

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,20 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <div class="bg-red-500 text-red-100">Hello from Tailwind again and again!!</div> -->
   <div id="home" style="display: grid; height: 80vh; place-items: center">
-    <div class="leading-relaxed text-7xl mt-16">
+    <div class="md:leading-relaxed text-2xl md:text-7xl mt-16">
       Nothing<br />[ worth having ]<br />comes easy
     </div>
   </div>
-  <div id="projects">
-    Projects
-    <div id="dynamic-projects">
-      Dynamic Projects
-    </div>
-    <div id="static-projects">
-      Static Projects
-    </div>
-  </div>
-  <div id="about">
-    About Me
-  </div>
-  <div id="contact">
-    My Contact Info
-  </div>
+  <Projects id="projects"></Projects>
+  <div id="about">About Me</div>
+  <div id="contact">My Contact Info</div>
   <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
 </template>
 
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 import Nav1 from "./components/Nav.vue";
+import Projects from "./components/Projects.vue";
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ src/components/Projects.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-gray-600 body-font py-16">
+    <h2 class="text-2xl">Projects</h2>
+    <div class="container px-5 py-4 mx-auto flex flex-wrap flex-col">
+      <div class="flex mx-auto flex-wrap mb-20">
+          <a v-for="i in tabs" :key="i.link" :class="`cursor-pointer sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none tracking-wider ${currentTab == i.value ? 'bg-gray-100 border-indigo-500 text-indigo-500 rounded-t' : ' border-gray-200 hover:text-gray-900'}`" v-text="i.text" @click="currentTab = i.value"></a>
+      </div>
+      <div id="dynamic-projects">Dynamic Projects</div>
+      <div id="static-projects">Static Projects</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tabs: [
+                {text: "Websites", value: "websites"},
+                {text: "Tools", value: "tools"},
+                {text: "Open Source Contributions", value: "openSource"},
+                {text: "Posters", value: "posters"}
+            ],
+            currentTab: "websites"
+        }
+    }
+};
+</script>
+
+<style>
+</style>
