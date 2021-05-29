1687d1f6a476f545e745b3c5e12cc24b75ea4158
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2021-05-29 22:31:44 +0530

    started working on the svg path

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Nav1></Nav1>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <img alt="Vue logo" src="./assets/logo.svg" /> -->
   <!-- <div class="bg-red-500 text-red-100">Hello from Tailwind again and again!!</div> -->
   <div id="home" style="display: grid; height: 80vh; place-items: center">
     <div class="md:leading-relaxed text-2xl md:text-7xl mt-16">
@@ -9,6 +9,7 @@
   </div>
   <Projects id="projects"></Projects>
   <div id="about">About Me</div>
+  <Historypath ></Historypath>
   <div id="contact">My Contact Info</div>
   <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
 </template>
@@ -17,6 +18,7 @@
 // import HelloWorld from './components/HelloWorld.vue'
 import Nav1 from "./components/Nav.vue";
 import Projects from "./components/Projects.vue";
+import Historypath from "./components/Historypath.vue";
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
